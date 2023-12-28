78bf8b07913d9fbddaea50b25a8d09b11b76e0f4
K-Musty <kmustapha9564@gmail.com>
2023-12-28 14:33:40 -0800

    malloc

--- practice/malloc.c
+++ practice/malloc.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * This is meant for small malloc program
+ */
+int main(void)
+{
+	int k;
+	int point, i, j;
+
+	printf("Enter number of memory\n");
+	scanf("%d\n", &n);
+	point = (int *)malloc(sizeof(int) * n);
+}
