dae13b7f0e4e45cc72e87607080512d15fc5d249
K-Musty <kmustapha9564@gmail.com>
2023-12-29 14:28:50 -0800

    this commit would update arg.c

--- practice/arg.c
+++ practice/arg.c
@@ -7,4 +7,14 @@ int main(int argc, char* argv[])
 	int sum = 0;
 	printf("argc = %d\n", argc);
 	printf("let's see what is in argv[]\n");
+	if (argc > 1)
+	{
+		for(i = 0; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+			sum = sum + atoi(argv[i]);
+		}
+		printf("Total = %d\n", sum);
+	}
+	return (0);
 }
