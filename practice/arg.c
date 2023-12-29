6f6e33c554959b7bd41ab1bf027f51a663fadb8b
K-Musty <kmustapha9564@gmail.com>
2023-12-29 14:24:38 -0800

    this commit would push arg.c file

---
+++ practice/arg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int i = 0;
+	int sum = 0;
+	printf("argc = %d\n", argc);
+	printf("let's see what is in argv[]\n");
+}
